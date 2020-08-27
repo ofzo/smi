152ed906d48220ae7f54b0b28919fca5fd108b18
roadup <roadup.inbox@gmail.com>
2020-08-27 15:30:35 +0800

    update js flow

--- src/Resource.js.js
+++ src/Resource.js.js
@@ -13,86 +13,63 @@ module.exports = class JsResource extends Resource {
     complier() {
         try {
             const self = this
-            this.content = babel.transformSync(this.source.toString(), {
-                plugins: [evnReplace, "minify-dead-code-elimination", "minify-constant-folding",
+            const code = babel.transformSync(this.source.toString(), {
+                plugins: [evnReplace, "minify-dead-code-elimination", "minify-constant-folding"]
+            }).code
+            this.content = babel.transformSync(code, {
+                plugins: [
                     {
                         visitor: {
-                            ImportDeclaration(path) {
-                                const value = path.node.source.value
-                                self.resolve(value, self.requires)
+                            ImportDeclaration: {
+                                exit(path) {
+                                    const value = path.node.source.value
+                                    self.resolve(value, self.requires)
+                                }
                             },
-                            CallExpression(path) {
-                                // @ts-ignore
-                                if (path.node.callee.name === "require") {
+                            // ImportDeclaration(path) {
+                            //     const value = path.node.source.value
+                            //     self.resolve(value, self.requires)
+                            // },
+                            CallExpression: {
+                                exit(path) {
                                     // @ts-ignore
-                                    const module = path.node.arguments[0].value
-                                    // @ts-ignore
-                                    self.resolve(module, self.requires)
-                                    return
-                                }
-                                // @ts-ignore
-                                if (path.node.callee.name === "page") {
-                                    // @ts-ignore
-                                    const page = path.node.arguments[0] && path.node.arguments[0].value
-                                    if (typeof page === "string") {
+                                    if (path.node.callee.name === "require") {
                                         // @ts-ignore
-                                        path.replaceWith(types.valueToNode(page.replace(/^@/, "/")))
-                                        self.resolve(page, self.pages)
-                                    } else {
-                                        console.error("[无效引用] ".red, self.path.replace(process.cwd(), ".") + ":" + path.node.loc.start.line, path.toString())
-                                        path.replaceWith(path.node.arguments[0])
-                                    }
-                                    return
-                                }
-                                // @ts-ignore
-                                if (path.node.callee.name === "file") {
-                                    // @ts-ignore
-                                    const file = path.node.arguments[0].value
-                                    if (typeof file === "string") {
+                                        const module = path.node.arguments[0].value
                                         // @ts-ignore
-                                        path.replaceWith(types.valueToNode(file.replace(/^@/, "/")))
-                                        self.resolve(file, self.requires)
-                                    } else {
-                                        console.error("[无效引用] ".red, self.path.replace(process.cwd(), ".") + ":" + path.node.loc.start.line, path.toString())
-                                        path.replaceWith(path.node.arguments[0])
+                                        self.resolve(module, self.requires)
+                                        return
                                     }
-                                    return
+                                    // @ts-ignore
+                                    if (path.node.callee.name === "page") {
+                                        // @ts-ignore
+                                        const page = path.node.arguments[0] && path.node.arguments[0].value
+                                        if (typeof page === "string") {
+                                            // @ts-ignore
+                                            path.replaceWith(types.valueToNode(page.replace(/^@/, "/")))
+                                            self.resolve(page, self.pages)
+                                        } else {
+                                            console.error("[无效引用] ".red, self.path.replace(process.cwd(), ".") + ":" + path.node.loc.start.line, path.toString())
+                                            path.replaceWith(path.node.arguments[0])
+                                        }
+                                        return
+                                    }
+                                    // @ts-ignore
+                                    if (path.node.callee.name === "file") {
+                                        // @ts-ignore
+                                        const file = path.node.arguments[0].value
+                                        if (typeof file === "string") {
+                                            // @ts-ignore
+                                            path.replaceWith(types.valueToNode(file.replace(/^@/, "/")))
+                                            self.resolve(file, self.requires)
+                                        } else {
+                                            console.error("[无效引用] ".red, self.path.replace(process.cwd(), ".") + ":" + path.node.loc.start.line, path.toString())
+                                            path.replaceWith(path.node.arguments[0])
+                                        }
+                                        return
+                                    }
                                 }
                             }
-                        }
-                    }, {
-                        visitor: {
-                            // IfStatement: {
-                            //     exit(path) {
-                            //         const test = path.node.test
-                            //         if (types.isBinaryExpression(test)) {
-                            //             const left = path.node.test.left.value
-                            //             if (typeof left === "undefined") return
-                            //             const right = path.node.test.right.value
-                            //             if (typeof right === "undefined") return
-                            //             const operator = path.node.test.operator
-                            //             if (operator === "==")
-                            //                 // eslint-disable-next-line eqeqeq
-                            //                 if (left != right) path.remove()
-                            //                 else path.replaceWithMultiple(path.node.consequent.body)
-                            //             else if (operator === "===")
-                            //                 if (left !== right) path.remove()
-                            //                 else path.replaceWithMultiple(path.node.consequent.body)
-                            //         } else if (
-                            //             types.isStringLiteral(test) ||
-                            //             types.isNumericLiteral(test) ||
-                            //             types.isNullLiteral(test) ||
-                            //             types.isBigIntLiteral(test) ||
-                            //             types.isBooleanLiteral(test)
-                            //         ) {
-                            //             if (!!test.value) {
-                            //                 path.replaceWithMultiple(path.node.consequent.body)
-                            //             } else {
-                            //                 path.remove()
-                            //             }
-                            //         }
-                            //     }
-                            // }
                         }
                     }
                 ]

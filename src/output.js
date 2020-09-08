841073a127eae2352448fdde645d3fc6f29c3201
roadup <roadup.inbox@gmail.com>
2020-09-08 18:34:03 +0800

    fix output dir

--- src/output.js
+++ src/output.js
@@ -4,9 +4,10 @@ const dist = path.resolve(root, "dist")
 
 const outputIndex = process.argv.findIndex(arg => arg === "-o")
 let outputDir = "dist"
-if (outputIndex) {
+if (outputIndex > 0) {
     console.log("构建文件将输出到：", process.argv[outputIndex + 1])
     outputDir = process.argv[outputIndex + 1]
+    process.argv.splice(outputIndex, 2)
 }
 module.exports = {
     outputPath: dist,
